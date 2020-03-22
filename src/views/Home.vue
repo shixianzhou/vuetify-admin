e56f8bd02be478456336cf692455375053a4bddd
shixianzhou <china_zhou@live.com>
2020-03-22 09:31:11 +0800

    alter a design

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,48 +4,34 @@
       v-model="primaryDrawer.model"
       :clipped="primaryDrawer.clipped"
       :floating="primaryDrawer.floating"
-      :mini-variant="primaryDrawer.mini"
-      :permanent="primaryDrawer.type === 'permanent'"
-      :temporary="primaryDrawer.type === 'temporary'"
+      :mini-variant="$store.state.primaryDrawer.mini"
       app
       overflow
     >
-      <v-img
-        src="https://cdn.vuetifyjs.com/images/lists/ali.png"
-        height="160px"
-        dark
-      >
-        <v-row class="fill-height">
-          <v-card-title>
-            <v-btn
-              dark
-              icon
-            >
-              <v-icon>mdi-chevron-left</v-icon>
-            </v-btn>
-            <v-btn
-              dark
-              icon
-              class="mr-4"
-            >
-              <v-icon>mdi-pencil</v-icon>
-            </v-btn>
-          </v-card-title>
+       <v-list>
+        <v-list-item>
+          <v-list-item-avatar>
+            <v-img src="https://cdn.vuetifyjs.com/images/john.png"></v-img>
+          </v-list-item-avatar>
+        </v-list-item>
 
-          <v-card-title class="white--text pl-12 pt-2">
-            <div class="display-1 pl-12 pt-12">Ali Conners</div>
-          </v-card-title>
-        </v-row>
-      </v-img>
+        <v-list-item link>
+          <v-list-item-content>
+            <v-list-item-title class="title">John Leider</v-list-item-title>
+            <v-list-item-subtitle>john@vuetifyjs.com</v-list-item-subtitle>
+          </v-list-item-content>
+
+          <v-list-item-action>
+            <v-icon>mdi-menu-down</v-icon>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
       <v-divider></v-divider>
       <v-list
         nav
         dense
       >
-        <v-list-item-group
-          v-model="item"
-          color="primary"
-        >
+        <v-list-item-group v-model="item" color="primary">
           <v-list-item
             v-for="(item, i) in items"
             :key="i"
@@ -54,7 +40,7 @@
               <v-icon v-text="item.icon"></v-icon>
             </v-list-item-icon>
 
-            <v-list-item-content @click="goRoute(item.text)">
+            <v-list-item-content>
               <v-list-item-title v-text="item.text"></v-list-item-title>
             </v-list-item-content>
           </v-list-item>
@@ -66,77 +52,78 @@
       :clipped-left="primaryDrawer.clipped"
       app
     >
-      <v-app-bar-nav-icon
-        v-if="primaryDrawer.type !== 'permanent'"
-        @click.stop="primaryDrawer.model = !primaryDrawer.model"
-      />
+      <v-app-bar-nav-icon @click.stop="primaryDrawer.model = !primaryDrawer.model" />
       <v-toolbar-title>Vuetify</v-toolbar-title>
     </v-app-bar>
 
     <v-content>
       <v-container fluid>
-        <router-view />
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col cols="10">
+            <v-card>
+              <v-card-text>
+                <v-row>
+                  <v-col
+                    cols="12"
+                    md="6"
+                  >
+                    <span>Scheme</span>
+                    <v-switch
+                      v-model="$vuetify.theme.dark"
+                      primary
+                      label="Dark"
+                    />
+                    <v-switch
+                      v-model="primaryDrawer.mini"
+                      label="Mini"
+                      primary
+                    />
+                  </v-col>
+                  <v-col
+                    cols="12"
+                    md="6"
+                  >
+
+                  </v-col>
+                </v-row>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn text>Cancel</v-btn>
+                <v-btn
+                  text
+                  color="primary"
+                >Submit</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
       </v-container>
     </v-content>
-
-    <v-footer
-      :inset="footer.inset"
-      app
-    >
-      <span class="px-4">&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
   </v-app>
 </template>
 
 <script>
+import { mapState } from "vuex";
 export default {
   data: () => ({
-    footer: {
-      inset: false
-    },
-    dialog: false,
-    drawer: null,
-    primaryDrawer: {
-      model: null,
-      type: "default (no property)",
-      clipped: false,
-      floating: false,
-      mini: false
-    },
-    items: [
-      { icon: "mdi-contacts", text: "about" },
-      { icon: "mdi-history", text: "Frequently contacted" },
-      { icon: "mdi-content-copy", text: "Duplicates" },
-      {
-        icon: "mdi-chevron-up",
-        "icon-alt": "mdi-chevron-down",
-        text: "Labels",
-        model: true,
-        children: [{ icon: "mdi-plus", text: "Create label" }]
-      },
-      {
-        icon: "mdi-chevron-up",
-        "icon-alt": "mdi-chevron-down",
-        text: "More",
-        model: false,
-        children: [
-          { text: "Import" },
-          { text: "Export" },
-          { text: "Print" },
-          { text: "Undo changes" },
-          { text: "Other contacts" }
-        ]
-      },
-      { icon: "mdi-cellphone-link", text: "App downloads" },
-      { icon: "mdi-keyboard", text: "Go to the old version" },
-      { icon: "mdi-settings", text: "Settings" },
-      { icon: "mdi-help-circle", text: "Help" }
-    ]
+	  item:'',
+	  items: [
+        { icon: 'mdi-contacts', text: 'Contacts' },
+        { icon: 'mdi-history', text: 'Frequently contacted' },
+        { icon: 'mdi-content-copy', text: 'Duplicates' },
+        { icon: 'mdi-settings', text: 'Settings' },
+        { icon: 'mdi-message', text: 'Send feedback' },
+        { icon: 'mdi-help-circle', text: 'Help' },
+        { icon: 'mdi-cellphone-link', text: 'App downloads' },
+        { icon: 'mdi-keyboard', text: 'Go to the old version' },
+      ],
   }),
-  methods: {
-    goRoute: function(routeName) {
-      this.$router.push("/" + routeName.toLowerCase());
-    }
+  computed: {
+    ...mapState(["primaryDrawer"])
   }
 };
 </script>
